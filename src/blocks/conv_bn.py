d11bb528e07f32e9a09b0f075b0717b2a23108b5
barkin <63337752+barkinadiguzel@users.noreply.github.com>
2026-04-21 11:17:27 +0300

    Create conv_bn.py

---
+++ src/blocks/conv_bn.py
@@ -0,0 +1,10 @@
+import torch.nn as nn
+
+class ConvBN(nn.Module):
+    def __init__(self, in_c, out_c, k=3, s=1, p=1):
+        super().__init__()
+        self.conv = nn.Conv2d(in_c, out_c, k, s, p, bias=False)
+        self.bn = nn.BatchNorm2d(out_c)
+
+    def forward(self, x):
+        return self.bn(self.conv(x))
